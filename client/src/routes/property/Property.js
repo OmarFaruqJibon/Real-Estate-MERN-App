0cfb9a5b273072be82b714192aef74d1946948d7
unknown <jibon602947@gmail.com>
2025-06-11 13:20:40 +0600

    single page ui

--- client/src/routes/property/Property.js
+++ client/src/routes/property/Property.js
@@ -66,51 +66,95 @@ const Property = ({ id }) => {
     };
 
     return (
-
-
         <div className="singlePage">
+
+
             <div className="details">
+
                 <div className="wrapper">
+
                     <Slider images={post?.images} />
 
                     <div className="info">
+
+
+                        {/* TITLE, LOCATION AND PRICE */}
                         <div className="top">
+
                             <div className="post">
 
+                                {/* TITLE & ADRESS */}
                                 <h2>{post?.title}</h2>
                                 <div className="address">
                                     <img src="https://i.postimg.cc/mg8RNbwp/location.png" alt="location" />
                                     <span>{post?.address}</span>
                                 </div>
-                                <div className="price">
-                                    <span>$ {post?.price}</span>
-                                    <Link to={"/profile"}>
-                                        <button
-                                            onClick={() => handleSendMessage(post.userId)}
-                                        >
-                                            {/* <img src="https://i.postimg.cc/X74w1F2r/chat-1.png" alt="" /> */}
-                                            <SendHorizontal size={15} color='white' />
-                                            Chat with agent
-                                        </button>
-                                    </Link>
+                            </div>
+
+                            {/* PRICE */}
+                            <div className="price">
+                                <span>BDT {post?.price}</span>
+                            </div>
+                        </div>
+
+
+
+                        <div className="property-sizes">
+                            <div className="wrapper">
+
+                                <p className="title">Sizes</p>
+                                <div className="sizes">
+                                    <div className="size">
+                                        <img src="https://i.postimg.cc/zD6r0tT5/room.png" alt="" />
+                                        <span>{post?.postDetail.size} sqft</span>
+                                    </div>
+                                    <div className="size">
+                                        <img src="https://i.postimg.cc/T14h90rB/bed-1.png" alt="" />
+                                        <span>{post?.bedroom} beds</span>
+                                    </div>
+                                    <div className="size">
+                                        <img src="https://i.postimg.cc/ZRYRz2H1/bathroom-1.png" alt="" />
+                                        <span>{post?.bathroom} baths</span>
+                                    </div>
+                                </div>
+
+                                <p className="title">Nearby Places</p>
+                                <div className="listHorizontal">
+                                    <div className="feature">
+                                        <img src="https://i.postimg.cc/T3XXXKk4/school.png" alt="" />
+                                        <div className="featureText">
+                                            <span>School</span>
+                                            <p>{post?.postDetail.school}m away</p>
+                                        </div>
+                                    </div>
+                                    <div className="feature">
+                                        <img src="https://i.postimg.cc/K8rhcmwv/bus-stop.png" alt="" />
+                                        <div className="featureText">
+                                            <span>Bus Stop</span>
+                                            <p>{post?.postDetail.bus}m away</p>
+                                        </div>
+                                    </div>
+                                    <div className="feature">
+                                        <img src="https://i.postimg.cc/tC6Hf3z6/restuarant.png" alt="" />
+                                        <div className="featureText">
+                                            <span>Hospital</span>
+                                            <p>{post?.postDetail.hospital}m away</p>
+                                        </div>
+                                    </div>
                                 </div>
 
                             </div>
-
-                            {/* Post creator information */}
-                            <div className="user">
-                                <img src={post?.user.avatar} alt="" />
-                                <span>{post?.user.username}</span>
-                            </div>
-
                         </div>
 
-                        <div className="bottom"
+
+
+                        {/* property description */}
+                        {/* <div className="property-description"
                             dangerouslySetInnerHTML={
                                 {
                                     __html: DOMPurify.sanitize(post?.postDetail.description),
                                 }}>
-                        </div>
+                        </div> */}
 
                     </div>
                 </div>
@@ -118,85 +162,12 @@ const Property = ({ id }) => {
 
 
 
-            <div className="features">
+
+
+
+            {/* CONTAINS MAP, SELLER INFORMATION */}
+            <div className="others-info">
                 <div className="wrapper">
-
-                    <p className="title">General</p>
-                    <div className="listVertical">
-
-                        <div className="feature">
-                            <img src="https://i.postimg.cc/rFGfKjCM/utilities-1.png" alt="" />
-                            <div className="featureText">
-                                <span>Utilities</span>
-                                {post?.postDetail.utilities === "owner" ? (
-                                    <p>Owner is responsible</p>
-                                ) : (
-                                    <p>Tenant is responsible</p>
-                                )}
-                            </div>
-                        </div>
-
-                        <div className="feature">
-                            <img src="https://i.postimg.cc/qgVW0FG3/hand.png" alt="" />
-                            <div className="featureText">
-                                <span>Pet Policy</span>
-                                {post?.postDetail.policy === "allowed" ? (
-                                    <p>Pets Allowed</p>
-                                ) : (
-                                    <p>Pets not Allowed</p>
-                                )}
-                            </div>
-                        </div>
-
-                        <div className="feature">
-                            <img src="https://i.postimg.cc/qgVW0FG3/hand.png" alt="" />
-                            <div className="featureText">
-                                <span>Property Fees</span>
-                                <p>{post?.postDetail.fees}</p>
-                            </div>
-                        </div>
-                    </div>
-
-                    <p className="title">Sizes</p>
-                    <div className="sizes">
-                        <div className="size">
-                            <img src="https://i.postimg.cc/zD6r0tT5/room.png" alt="" />
-                            <span>{post?.postDetail.size} sqft</span>
-                        </div>
-                        <div className="size">
-                            <img src="https://i.postimg.cc/T14h90rB/bed-1.png" alt="" />
-                            <span>{post?.bedroom} beds</span>
-                        </div>
-                        <div className="size">
-                            <img src="https://i.postimg.cc/ZRYRz2H1/bathroom-1.png" alt="" />
-                            <span>{post?.bathroom} baths</span>
-                        </div>
-                    </div>
-
-                    <p className="title">Nearby Places</p>
-                    <div className="listHorizontal">
-                        <div className="feature">
-                            <img src="https://i.postimg.cc/T3XXXKk4/school.png" alt="" />
-                            <div className="featureText">
-                                <span>School</span>
-                                <p>{post?.postDetail.school}m away</p>
-                            </div>
-                        </div>
-                        <div className="feature">
-                            <img src="https://i.postimg.cc/K8rhcmwv/bus-stop.png" alt="" />
-                            <div className="featureText">
-                                <span>Bus Stop</span>
-                                <p>{post?.postDetail.bus}m away</p>
-                            </div>
-                        </div>
-                        <div className="feature">
-                            <img src="https://i.postimg.cc/tC6Hf3z6/restuarant.png" alt="" />
-                            <div className="featureText">
-                                <span>Hospital</span>
-                                <p>{post?.postDetail.hospital}m away</p>
-                            </div>
-                        </div>
-                    </div>
 
                     <p className="title">Location</p>
                     <div className="mapContainer">
@@ -216,6 +187,9 @@ const Property = ({ id }) => {
                             </button>
                         </Link>
 
+
+
+                        {/* SAVE POST BUTTON */}
                         {/* <button
                             onClick={handleSave}
                             style={{
@@ -230,6 +204,12 @@ const Property = ({ id }) => {
 
                 </div>
             </div>
+
+
+
+
+
+
         </div>
     );
 };
