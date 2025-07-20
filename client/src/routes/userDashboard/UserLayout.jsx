692e340c9aad1756b375d97b2dcc169d5df799fb
OmarFaruqJibon <omarfarukjibon418@gmail.com>
2025-07-20 12:07:12 +0600

    user  dashboard

--- client/src/routes/userDashboard/UserLayout.jsx
+++ client/src/routes/userDashboard/UserLayout.jsx
@@ -1,5 +1,4 @@
-import React from "react";
-import "./UserLayoutStyle.scss";
+import React, { useState } from "react";
 import {
   Box,
   CssBaseline,
@@ -8,51 +7,149 @@ import {
   ListItem,
   ListItemIcon,
   ListItemText,
+  AppBar,
+  Toolbar,
+  IconButton,
+  Typography,
+  useMediaQuery,
+  useTheme,
+  Tooltip,
 } from "@mui/material";
-import { Home, Person } from "@mui/icons-material";
-import { Outlet, Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
+import MenuIcon from "@mui/icons-material/Menu";
+import { Dashboard, Home, Person } from "@mui/icons-material";
+import "../adminDashboard/AdminDashboard.scss";
 
-const drawerWidth = 270;
+const fullDrawerWidth = 250;
+const collapsedDrawerWidth = 72;
 
 const UserLayout = () => {
+  const theme = useTheme();
+  const isMobile = useMediaQuery(theme.breakpoints.down("md"));
+
+  const [mobileOpen, setMobileOpen] = useState(false);
+  const [collapsed, setCollapsed] = useState(false); // for desktop sidebar text toggle
+
+  const handleDrawerToggle = () => {
+    if (isMobile) {
+      setMobileOpen(!mobileOpen);
+    } else {
+      setCollapsed(!collapsed);
+    }
+  };
+
+  const drawerWidth = isMobile
+    ? fullDrawerWidth
+    : collapsed
+    ? collapsedDrawerWidth
+    : fullDrawerWidth;
+
+  const drawer = (
+    <List sx={{ color: "white", marginTop: "17px" }}>
+      {[
+        { label: "Home", icon: <Home sx={{ color: "white" }} />, to: "/" },
+        {
+          label: "Overview",
+          icon: <Dashboard sx={{ color: "white" }} />,
+          to: "",
+        },
+        {
+          label: "My Profile",
+          icon: <Person sx={{ color: "white" }} />,
+          to: "/dashboard/profile",
+        },
+        {
+          label: "My Listings",
+          icon: <Person sx={{ color: "white" }} />,
+          to: "/dashboard/posts",
+        },
+      ].map(({ label, icon, to }) => (
+        <Tooltip
+          key={label}
+          title={collapsed && !isMobile ? label : ""}
+          placement="right"
+        >
+          <ListItem button component={Link} to={to}>
+            <ListItemIcon sx={{ minWidth: 40, mb: 2 }}>{icon}</ListItemIcon>
+            {(!collapsed || isMobile) && (
+              <ListItemText sx={{ minWidth: 40, mb: 2 }} primary={label} />
+            )}
+          </ListItem>
+        </Tooltip>
+      ))}
+    </List>
+  );
+
   return (
     <Box sx={{ display: "flex" }}>
       <CssBaseline />
 
-      {/* Sidebar */}
+      {/* AppBar always shown */}
+      <AppBar
+        position="fixed"
+        sx={{
+          zIndex: (theme) => theme.zIndex.drawer + 1,
+          backgroundColor: "#121621",
+          minHeight: "75px",
+        }}
+      >
+        <Toolbar sx={{ minHeight: "75px !important" }}>
+          <IconButton
+            color="inherit"
+            edge="start"
+            onClick={handleDrawerToggle}
+            sx={{ mr: 2 }}
+          >
+            <MenuIcon />
+          </IconButton>
+          <Typography variant="h6" noWrap>
+            Dashboard
+          </Typography>
+        </Toolbar>
+      </AppBar>
+
+      {/* Sidebar Drawer */}
       <Drawer
-        variant="permanent"
+        variant={isMobile ? "temporary" : "permanent"}
+        open={isMobile ? mobileOpen : true}
+        onClose={() => setMobileOpen(false)}
+        ModalProps={{ keepMounted: true }}
         sx={{
           width: drawerWidth,
+          flexShrink: 0,
           [`& .MuiDrawer-paper`]: {
             width: drawerWidth,
             boxSizing: "border-box",
-            mt: "75px", // if there's a global navbar
+            mt: isMobile ? "64px" : "75px",
+            backgroundColor: "#1d1d1dff",
+            color: "white",
+            overflowX: "hidden",
+            transition: theme.transitions.create("width", {
+              easing: theme.transitions.easing.sharp,
+              duration: theme.transitions.duration.enteringScreen,
+            }),
           },
         }}
       >
-        {/* <Toolbar /> */}
-
-        <List sx={{ color: "white", marginTop: "17px" }}>
-          <ListItem button component={Link} to="/dashboard/profile">
-            <ListItemIcon>
-              <Person sx={{ color: "white" }} />
-            </ListItemIcon>
-            <ListItemText primary="My Profile" />
-          </ListItem>
-
-          <ListItem button component={Link} to="/dashboard/posts">
-            <ListItemIcon>
-              <Home sx={{ color: "white" }} />
-            </ListItemIcon>
-            <ListItemText primary="My Listings" />
-          </ListItem>
-        </List>
+        {drawer}
       </Drawer>
 
-      {/* Main content */}
-      <Box component="main" sx={{ flexGrow: 1, p: 3, ml: "0px", mt: "10px" }}>
-        {/* <Toolbar /> */}
+      {/* Main Content */}
+      <Box
+        component="main"
+        sx={{
+          flexGrow: 1,
+          p: 3,
+          width: {
+            xs: "100%",
+            md: `calc(100% - ${drawerWidth}px)`,
+          },
+          transition: theme.transitions.create(["margin", "width"], {
+            easing: theme.transitions.easing.sharp,
+            duration: theme.transitions.duration.leavingScreen,
+          }),
+        }}
+      >
         <Outlet />
       </Box>
     </Box>
