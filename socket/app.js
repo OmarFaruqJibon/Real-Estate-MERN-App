bd93cf1e45a29d965de4613015923ec7cd3e90ab
OmarFaruqJibon <omarfarukjibon418@gmail.com>
2025-07-23 00:08:54 +0600

    socket code update for deploy

--- socket/app.js
+++ socket/app.js
@@ -1,55 +1,61 @@
 import { Server } from "socket.io";
+import http from "http";
+import dotenv from "dotenv";
 
-const io = new Server({
-    cors: {
-        origin: "http://localhost:3000",
-    },
+dotenv.config();
+
+const PORT = process.env.PORT || 4000;
+
+// Create HTTP server only for Socket.IO binding
+const server = http.createServer();
+
+const io = new Server(server, {
+  cors: {
+    origin: process.env.CLIENT_URL || "http://localhost:3000",
+    methods: ["GET", "POST"],
+  },
 });
 
 let onlineUser = [];
 
 const addUser = (userId, socketId) => {
-    const userExits = onlineUser.find((user) => user.userId === userId);
-    if (!userExits) {
-        onlineUser.push({ userId, socketId });
-    }
+  const userExists = onlineUser.find((user) => user.userId === userId);
+  if (!userExists) {
+    onlineUser.push({ userId, socketId });
+  }
 };
 
 const removeUser = (socketId) => {
-    onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
+  onlineUser = onlineUser.filter((user) => user.socketId !== socketId);
 };
 
 const getUser = (userId) => {
-    return onlineUser.find((user) => user.userId === userId);
+  return onlineUser.find((user) => user.userId === userId);
 };
 
-
-
 io.on("connection", (socket) => {
-    socket.on("newUser", (userId) => {
-        addUser(userId, socket.id);
-    });
+  console.log("User connected:", socket.id);
 
-    socket.on("sendMessage", ({ receiverId, data }) => {
+  socket.on("newUser", (userId) => {
+    addUser(userId, socket.id);
+  });
 
-        const receiver = getUser(receiverId);
+  socket.on("sendMessage", ({ receiverId, data }) => {
+    const receiver = getUser(receiverId);
+    if (receiver) {
+      io.to(receiver.socketId).emit("getMessage", data);
+      console.log("Message sent to:", receiverId);
+    } else {
+      console.log("Receiver not online:", receiverId);
+    }
+  });
 
-        if (!receiver) {
-            console.error("Receiver not found for ID:", receiverId);
-            return;
-        }
-
-        io.to(receiver.socketId).emit("getMessage", data);
-
-        console.log(data);
-    });
-
-    socket.on("disconnect", () => {
-        removeUser(socket.id);
-    });
+  socket.on("disconnect", () => {
+    console.log("User disconnected:", socket.id);
+    removeUser(socket.id);
+  });
 });
 
-
-
-
-io.listen("4000");
+server.listen(PORT, () => {
+  console.log(`Darhub Socket server running on port ${PORT}`);
+});
