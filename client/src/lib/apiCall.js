73a884eb723b72cf23dc0ab2a2551824df989649
OmarFaruqJibon <omarfarukjibon418@gmail.com>
2025-07-23 15:51:27 +0600

    remove .env

--- client/src/lib/apiCall.js
+++ client/src/lib/apiCall.js
@@ -2,6 +2,7 @@ import axios from "axios";
 
 const apiCall = axios.create({
   // baseURL: "http://localhost:8800/api",
+  // baseURL: process.env.BACKEND_API_URL || "http://localhost:8800/api",
   baseURL: "https://darhub-server.onrender.com/api",
   withCredentials: true,
 });
