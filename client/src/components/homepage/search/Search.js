0b37bcae63608660abf9ae58134476ea1b7e7910
OmarFaruqJibon <omarfarukjibon418@gmail.com>
2026-01-21 11:41:47 +0300

    searchbar update

--- client/src/components/homepage/search/Search.js
+++ client/src/components/homepage/search/Search.js
@@ -1,4 +1,3 @@
-// client\src\components\homepage\search\Search.js
 import React, { useState } from "react";
 import "./Search.scss";
 import { Link } from "react-router-dom";
@@ -28,53 +27,61 @@ const Search = () => {
           <button
             key={type}
             onClick={() => switchType(type)}
-            className={query.type === type ? "active" : ""}
+            className={`type-btn ${query.type === type ? "active" : ""}`}
           >
             {type} Property
           </button>
         ))}
       </div>
 
-      <form className="form" action="">
-        <div className="input-item">
-          <label htmlFor="city">Location</label>
-          <input
-            type="text"
-            name="city"
-            id="city"
-            placeholder="City"
-            onChange={handleChange}
-          />
-        </div>
+      <form className="form">
+        <div className="input-row">
+          <div className="input-item">
+            <label htmlFor="city">Location</label>
+            <input
+              type="text"
+              name="city"
+              id="city"
+              placeholder="City Location"
+              onChange={handleChange}
+            />
+          </div>
 
-        <div className="input-item">
-          <label htmlFor="minPrice">Min Price</label>
-          <input
-            type="number"
-            name="minPrice"
-            id="minPrice"
-            placeholder="$100"
-            onChange={handleChange}
-          />
-        </div>
+          <div className="input-item">
+            <label htmlFor="minPrice">Min Price</label>
+            <div className="price-input">
+              <span className="currency">$</span>
+              <input
+                type="number"
+                name="minPrice"
+                id="minPrice"
+                placeholder="100"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
 
-        <div className="input-item">
-          <label htmlFor="maxPrice">Max Price</label>
-          <input
-            type="text"
-            name="maxPrice"
-            id="maxPrice"
-            placeholder="$15000"
-            onChange={handleChange}
-          />
-        </div>
+          <div className="input-item">
+            <label htmlFor="maxPrice">Max Price</label>
+            <div className="price-input">
+              <span className="currency">$</span>
+              <input
+                type="number"
+                name="maxPrice"
+                id="maxPrice"
+                placeholder="15000"
+                onChange={handleChange}
+              />
+            </div>
+          </div>
 
-        <div className="input-item-btn">
-          <Link
-            to={`/list?type=${query.type}&city=${query.city}&minPrice=${query.minPrice}&maxPrice=${query.maxPrice}`}
-          >
-            <input className="button" type="button" value="Search" />
-          </Link>
+          <div className="input-item-btn">
+            <Link
+              to={`/list?type=${query.type}&city=${query.city}&minPrice=${query.minPrice}&maxPrice=${query.maxPrice}`}
+            >
+              <button className="search-btn">Search</button>
+            </Link>
+          </div>
         </div>
       </form>
     </div>
